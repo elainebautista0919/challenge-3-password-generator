7c483d36af3ce0d5cdfc47f66f0534a3fa2d1fdd
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-10-23 15:48:39 -0400

    Added uppercase conversion

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Uppercase conversion
+space = [];
+
+var toUpper = function (x) {
+  return x.toUpperCase();
+
+alphabet2 = alphabet.map(toUpper);
+
+var get = document.querySelector("#generate");
+  
+get.addEventListener("click", function () {
+      ps = generatePassword();
+      document.getElementById("password").placeholder = ps;
+});
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
