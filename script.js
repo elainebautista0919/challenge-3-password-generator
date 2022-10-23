8450dcb0d08994574be1840c24d6311f80822b67
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-10-23 15:38:32 -0400

    Added input variables and values

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment code here
+// User input variables
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
+// Password variable values
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
