e9326fe6d1b3ec31fc7d3ff47c520383c5f4d3a6
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-10-23 16:11:54 -0400

    Added password generation function

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ var confirmLowerCase;
 // Password variable values
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Uppercase conversion
 space = [];
 
+var choices;
+
 var toUpper = function (x) {
   return x.toUpperCase();
+};
 
 alphabet2 = alphabet.map(toUpper);
 
@@ -25,7 +28,31 @@ get.addEventListener("click", function () {
       ps = generatePassword();
       document.getElementById("password").placeholder = ps;
 });
-  
+
+// Function for password generation
+function generatePassword() {
+
+  // Password length, number, special character, uppercase and lowercase confirmation
+enter = parseInt(prompt("How many characters would you like for your password? Please choose between 8 to 128 characters."));
+
+if (!enter) {
+  alert ("Please enter a value.");
+} else if (enter < 8 || enter > 128) {
+  enter = parseInt(prompt("Please enter a value between 8 to 128 characters.")
+} else {
+  confirmNumber = confirm("Will it contain number/s?");
+  confirmCharacter = confirm("Will it contain special character/s?");
+  confirmUpperCase = confirm("Will it contain uppercase letter/s?");
+  confirmLowerCase = confirm("Will it contain lower case letter/s?")
+};
+
+}
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
